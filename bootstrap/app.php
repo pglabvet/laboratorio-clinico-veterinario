7dbdc18f2d65998034dc44840743cd16bbd9dc1e
copipaste <jhoeldebrayquispe@gmail.com>
2026-02-10 22:23:05 -0400

    automatizacion de eliminar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,9 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule): void {
+        // Limpiar PDFs y charts con más de 2 meses de antigüedad (cada domingo a las 2:00 AM)
+        $schedule->command('cleanup:archivos-antiguos')->weeklyOn(0, '02:00');
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
