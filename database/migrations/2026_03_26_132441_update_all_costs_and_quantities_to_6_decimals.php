b8217e55850c5fb4a94022b39e2d989dc49168b4
jhoel <jhoeldebrayquispe@gmail.com>
2026-03-26 17:35:25 -0400

    se arreglo bug al registrar insumos

--- database/migrations/2026_03_26_132441_update_all_costs_and_quantities_to_6_decimals.php
+++ database/migrations/2026_03_26_132441_update_all_costs_and_quantities_to_6_decimals.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::table('movimientos_inventario', function (Blueprint $table) {
             $table->decimal('cantidad', 14, 6)->change();
-            $table->decimal('costo_total', 14, 6)->change();
+            $table->decimal('costo_total', 14, 6)->default(0)->change();
+            $table->decimal('costo_unitario', 14, 6)->default(0)->change();
         });
 
         Schema::table('lotes_inventario', function (Blueprint $table) {
@@ -22,9 +23,9 @@ return new class extends Migration
         });
 
         Schema::table('inventario_sucursal', function (Blueprint $table) {
-            $table->decimal('stock_actual', 14, 6)->change();
-            $table->decimal('stock_minimo', 14, 6)->change();
-            $table->decimal('costo_total', 14, 6)->change();
+            $table->decimal('stock_actual', 14, 6)->default(0)->change();
+            $table->decimal('stock_minimo', 14, 6)->default(0)->change();
+            $table->decimal('costo_total', 14, 6)->default(0)->change();
         });
 
         Schema::table('analisis_insumos', function (Blueprint $table) {
