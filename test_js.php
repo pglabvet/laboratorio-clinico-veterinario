643f30f44cd2587562e923f732f613fc50f85d77
copipaste <jhoeldebrayquispe@gmail.com>
2026-04-01 15:59:03 -0400

    fix js

--- test_js.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$str = "Línea 1\nLínea 2\n\"Comillas\" y 'Simples'";
-echo 'Js::from() output: ' . Illuminate\Support\Js::from($str) . PHP_EOL;
-echo 'json_encode() output: ' . json_encode($str) . PHP_EOL;
